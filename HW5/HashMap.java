ea1929dda7783f54cfb2a6253e5583d83df91984
clairecao <zongwanc@gmail.com>
2015-02-26 22:54:21 -0500

    hashmap

---
+++ HW5/HashMap.java
@@ -0,0 +1,62 @@
+import java.util.List;
+import java.util.Set;
+
+public class HashMap<K, V> implements HashMapInterface<K, V> {
+
+    // Do not make any new instance variables.
+    private MapEntry<K, V>[] table;
+    private int size;
+
+    /**
+     * Create a hash map with no entries.
+     */
+    public HashMap() {
+        // Initialize your hashtable here.
+    }
+
+    @Override
+    public V add(K key, V value) {
+
+    }
+
+    @Override
+    public V remove(K key) {
+
+    }
+
+    @Override
+    public V get(K key) {
+
+    }
+
+    @Override
+    public boolean contains(K key) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+
+    }
+
+    @Override
+    public MapEntry<K, V>[] toArray() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+
+    }
+
+    @Override
+    public List<V> values() {
+
+    }
+
+}
